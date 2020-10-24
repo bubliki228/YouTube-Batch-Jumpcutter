942adc53685a73c30fb99e570dee0023c88fc9b7
Ilya Tampio <53406289+Quakumei@users.noreply.github.com>
2020-10-24 15:09:01 +0300

    Update SetupScript.sh

--- SetupScript.sh
+++ SetupScript.sh
@@ -2,5 +2,6 @@
 git clone https://github.com/carykh/jumpcutter
 git clone https://github.com/Quakumei/YouTube-HD-Batch-Downloader
 pip install -r jumpcutter/requirements.txt
+pip install pytube3
 sudo apt-get install ffmpeg
 touch LINK_LIST.txt
